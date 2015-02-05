7be19e130a341b562b74a801bfefb1720d5c7d7c
t-sin <shinichi.tanaka45@gmail.com>
2015-02-05 15:08:16 +0900

    Adds a predicate about end-of-line availability for each implementations and eol names

--- src/eol.lisp
+++ src/eol.lisp
@@ -7,4 +7,43 @@
 (enable-annot-syntax)
 
 
-;; blah blah blah.
+@export
+(defun eol-available-p ()
+  ;; #+allegro
+  ;; #+lispworks
+  #+clisp t
+  #+ecl t
+  #+sbcl nil
+  #+ccl t
+  #+abcl t)
+
+
+@export
+(defun lf-keyword ()
+  ;; #+lispworks :lf
+  #+clisp :unix
+  ;; #+ecl :lf
+  ;; #+sbcl nil ; sbcl cannot treat line-breaks
+  #+ccl :unix
+  ;; #+abcl :lf
+  #-(and clisp ccl) :lf)
+
+@export
+(defun cr-keyword ()
+  ;; #+lispworks :cr
+  #+clisp :mac
+  ;; #+ecl :cr
+  ;; #+sbcl nil ; sbcl cannot treat line-breaks
+  #+ccl :macos
+  ;; #+abcl :cr
+  #-(and clisp ccl) :cr)
+
+@export
+(defun crlf-keyword ()
+  ;; #+lispworks :crlf
+  #+clisp :dos
+  ;; #+ecl :crlf
+  ;; #+sbcl nil ; sbcl cannot treat line-breaks
+  #+ccl :dos
+  ;; #+abcl :crlf
+  #-(and clisp ccl) :crlf)
