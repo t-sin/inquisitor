2f2a97ddc67a9db281b9683509a9b189dd59720f
t-sin <shinichi.tanaka45@gmail.com>
2015-02-06 15:26:29 +0900

    Adds external-format maker

--- src/inquisitor.lisp
+++ src/inquisitor.lisp
@@ -1,6 +1,20 @@
 (in-package :cl-user)
 (defpackage inquisitor
-  (:use :cl))
+  (:use :cl
+        :cl-annot))
 (in-package :inquisitor)
 
-;; blah blah blah.
+(enable-annot-syntax)
+
+
+@export
+(defun make-external-format (enc eol)
+  #+clisp (ext:make-encoding :charset enc
+                             :line-terminator eol)
+  #+ecl `(,enc ,eol)
+  #+sbcl enc
+  #+ccl (ccl:make-external-format :character-encoding enc
+                                  :line-termination eol)
+  #+abcl `(,enc :eol-style ,eol)
+  #-(or clisp ecl sbcl ccl abcl) (error "your implementation is not supported."))
+
