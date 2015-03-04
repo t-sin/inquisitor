c081b2e8af696bcb029594cd453867a331eba4ff
t-sin <shinichi.tanaka45@gmail.com>
2015-03-04 18:49:46 +0900

    Adds tests about end-of-line

--- t/inquisitor.lisp
+++ t/inquisitor.lisp
@@ -2,6 +2,7 @@
 (defpackage inquisitor-test
   (:use :cl
         :inquisitor
+        :inquisitor.keyword
         :prove))
 (in-package :inquisitor-test)
 
@@ -9,6 +10,22 @@
 
 (plan nil)
 
-;; blah blah blah.
+(deftest end-of-line
+  (flet ((test-eol (path eol)
+           (with-open-file (in (merge-pathnames path *load-truename*)
+                            :direction :input
+                            :element-type '(unsigned-byte 8))
+             (is (detect-end-of-line in) eol))))
+
+    (test-eol "dat/ja/utf8-cr.ja" (cr-keyword))
+    (test-eol "dat/ja/utf8-crlf.ja" (crlf-keyword))
+    (test-eol "dat/ja/utf8-lf.ja" (lf-keyword))
+
+    ;; if file has no newline then return LF
+    (test-eol "dat/ascii.txt" (lf-keyword))
+    (test-eol "dat/empty.txt" (lf-keyword))))
+
+
+(run-test-all)
 
 (finalize)
