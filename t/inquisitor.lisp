2b6be7f78512b9e9466a8db2d34ab0a0e8af5fd5
t-sin <shinichi.tanaka45@gmail.com>
2015-04-10 16:48:53 +0900

    Adds tests about encoding (JP). Note that surrogate pair test is NOT PASSED

--- t/inquisitor.lisp
+++ t/inquisitor.lisp
@@ -8,9 +8,9 @@
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :inquisitor)' in your Lisp.
 
-(plan nil)
+(plan 12)
 
-(deftest end-of-line
+(subtest "end-of-line"
   (flet ((test-eol (path eol)
            (with-open-file (in (merge-pathnames path *load-truename*)
                             :direction :input
@@ -26,6 +26,36 @@
     (test-eol "dat/empty.txt" (lf-keyword))))
 
 
-(run-test-all)
+;;;; encoding
+(defun test-enc (path scm enc)
+  (with-open-file (in (merge-pathnames path *load-truename*)
+                   :direction :input
+                   :element-type '(unsigned-byte 8))
+    (multiple-value-bind (encoding treatable)
+        (detect-encoding in scm)
+      (is encoding enc)
+      (when treatable
+        (diag (format nil " ; ~a cannot treat ~a"
+                      (lisp-implementation-type)
+                      encoding))))))
+
+(subtest "encoding -- jp"
+  (test-enc "dat/empty.txt" :jp (utf8-keyword))
+  (test-enc "dat/ascii.txt" :jp (utf8-keyword))
+
+  (test-enc "dat/ja/eucjp-lf.ja" :jp (eucj-keyword))
+
+  (test-enc "dat/ja/jis-lf.ja" :jp (iso-2022-jp-keyword))
+
+  (test-enc "dat/ja/sjis-crlf.ja" :jp (sjis-keyword))
+
+  (test-enc "dat/ja/utf16-be-lf.ja" :jp (ucs-2be-keyword))
+  (test-enc "dat/ja/utf16-le-lf.ja" :jp (ucs-2le-keyword))
+  (test-enc "dat/ja/utf16-lf.ja" :jp (utf16-keyword)) ; not UCS-2 because of surrogate pair
+
+  (test-enc "dat/ja/utf8-cr.ja" :jp (utf8-keyword))
+  (test-enc "dat/ja/utf8-crlf.ja" :jp (utf8-keyword))
+  (test-enc "dat/ja/utf8-lf.ja" :jp (utf8-keyword)))
+
 
 (finalize)
