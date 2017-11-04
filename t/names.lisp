111fb4ca0f387674513629492bd7b92fe5ba0d54
t-sin <shinichi.tanaka45@gmail.com>
2017-11-04 09:48:51 +0900

    Tests conversion from flexi-streams name to inquisitor name

--- t/names.lisp
+++ t/names.lisp
@@ -7,7 +7,7 @@
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :inquisitor)' in your Lisp.
 
-(plan 16)
+(plan 18)
 
 (is +available-encodings+
       ;; unicode
@@ -438,9 +438,11 @@
 (subtest "names on flexi-streams"
   (subtest "unicode"
     (is (dependent-name :utf-8 :flexi-name) :utf-8)
+    (is (independent-name :utf-8 :flexi-name) :utf-8)
     (is (dependent-name :ucs-2le :flexi-name) nil)
     (is (dependent-name :ucs-2be :flexi-name) nil)
-    (is (dependent-name :utf-16 :flexi-name) :utf-16))
+    (is (dependent-name :utf-16 :flexi-name) :utf-16)
+    (is (independent-name :utf-16 :flexi-name) :utf-16))
 
   (subtest "japanese"
     (is (dependent-name :iso-2022-jp :flexi-name) nil)
@@ -463,39 +465,50 @@
 
   (subtest "arabic"
     (is (dependent-name :iso-8859-6 :flexi-name) :iso-8859-6)
+    (is (independent-name :iso-8859-6 :flexi-name) :iso-8859-6)
     (is (dependent-name :cp1256 :flexi-name) nil))
 
   (subtest "greek"
     (is (dependent-name :iso-8859-7 :flexi-name) :iso-8859-7)
+    (is (independent-name :iso-8859-7 :flexi-name) :iso-8859-7)
     (is (dependent-name :cp1253 :flexi-name) nil))
 
   (subtest "hebrew"
     (is (dependent-name :iso-8859-8 :flexi-name) :iso-8859-8)
+    (is (independent-name :iso-8859-8 :flexi-name) :iso-8859-8)
     (is (dependent-name :cp1255 :flexi-name) nil))
 
   (subtest "turkish"
     (is (dependent-name :iso-8859-9 :flexi-name) :iso-8859-9)
+    (is (independent-name :iso-8859-9 :flexi-name) :iso-8859-9)
     (is (dependent-name :cp1254 :flexi-name) nil))
 
   (subtest "russian"
     (is (dependent-name :iso-8859-5 :flexi-name) :iso-8859-5)
+    (is (independent-name :iso-8859-5 :flexi-name) :iso-8859-5)
     (is (dependent-name :koi8-r :flexi-name) :koi8-r)
+    (is (independent-name :koi8-r :flexi-name) :koi8-r)
     (is (dependent-name :koi8-u :flexi-name) nil)
     (is (dependent-name :cp866 :flexi-name) nil)
     (is (dependent-name :cp1251 :flexi-name) nil))
 
   (subtest "polish"
     (is (dependent-name :iso-8859-2 :flexi-name) :iso-8859-2)
+    (is (independent-name :iso-8859-2 :flexi-name) :iso-8859-2)
     (is (dependent-name :cp1250 :flexi-name) nil))
 
   (subtest "baltic"
     (is (dependent-name :iso-8859-13 :flexi-name) :iso-8859-13)
+    (is (independent-name :iso-8859-13 :flexi-name) :iso-8859-13)
     (is (dependent-name :cp1257 :flexi-name) nil))
 
   (subtest "eol"
     (is (dependent-name :lf :flexi-name) :lf)
+    (is (independent-name :lf :flexi-name) :lf)
     (is (dependent-name :cr :flexi-name) :cr)
+    (is (independent-name :cr :flexi-name) :cr)
     (is (dependent-name :crlf :flexi-name) :crlf)))
+    (is (independent-name :crlf :flexi-name) :crlf)
 
 (subtest "if specified encodings is unicode?"
   (subtest "only unicode returns t"
